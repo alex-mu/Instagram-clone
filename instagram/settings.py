0f6076800bb8f8d404d6542721a558e1a9bab441
Alexander <alexndegwa46@gmail.com>
2022-03-07 17:43:21 +0300

    django heroku installed

--- instagram/settings.py
+++ instagram/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
     }
 }
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
